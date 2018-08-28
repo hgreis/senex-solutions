b0f7258dc5ed9bffb45819c3be37805b05ead231
hgreis <heikogreis@justmail.de>
2018-08-28 13:41:56 +0200

    update migration

---
+++ database/migrations/2018_08_07_101150_update_companies2.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCompanies2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('companies', function (Blueprint $table) {
+            $table->text('turnoverTax')->after('taxNumber')->nullable();
+            $table->text('fax')->after('phone')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropColumn('fax');
+            $table->dropColumn('turnoverTax');
+        });
+    }
+}
