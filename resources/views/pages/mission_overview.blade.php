8221decc317d46cc9e529def3995fc528167057f
hgreis <38749115+hgreis@users.noreply.github.com>
2018-11-23 15:19:23 +0100

    #05.06 update

--- resources/views/pages/mission_overview.blade.php
+++ resources/views/pages/mission_overview.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.main')
 @section('content')
     <h2>Auftragsübersicht #{{ $mission->id }}</h2><br> 
-    <h3>Touren-Details</h3><br>
+    <h3>Touren-Details</h3>
         <table style="width: 100%">
             <tr>
-                <th></th>
-                <th>Datum</th>
+                <th style="width: 60px"></th>
+                <th style="width: 100px">Datum</th>
                 <th>Name</th>
                 <th>Strasse</th>
                 <th>Ort</th>
                 <th>Bemerkung</th>
             </tr>
             <tr>
-                <td><b>START</b></td>
+                <td><b>START:</b></td>
                 <td>{{ $mission->startDatum }}</td>
                 <td>{{ $mission->startName }}</td>
                 <td>{{ $mission->startStrasse }}</td>
@@ -20,7 +20,7 @@
                 <td>{{ $mission->startBemerkung }}</td>
             </tr>
             <tr>
-                <td><b>ZIEL</b></td>
+                <td><b>ZIEL:</b></td>
                 <td>{{ $mission->zielDatum }}</td>
                 <td>{{ $mission->zielName }}</td>
                 <td>{{ $mission->zielStrasse }}</td>
@@ -28,6 +28,35 @@
                 <td>{{ $mission->zielBemerkung }}</td>
             </tr>
         </table><hr>
-    <h3>Fahrer-Details</h3><br>
-        
+    <h3>Fahrer-Details</h3>
+        <table>
+            <tr>
+                <td style="padding: 10px">
+                    <b>Fahrer: </b>{{ $mission->driver->name }}
+                </td>
+                <td style="padding: 10px">
+                    Telefon: {{ $mission->driver->phone }}
+                </td>
+                <td style="padding: 10px">
+                    Nummernschild: {{ $mission->driver->number_plate }}
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 10px">
+                    <b>Unternehmer: </b>{{ $mission->driver->contractor }}
+                </td>
+                <td style="padding: 10px">
+                    Anschrift: {{ $mission->driver->street }}, {{ $mission->driver->city }}
+                </td>
+                <td style="padding: 10px">
+                    Emailadresse: {{ $mission->driver->email }}
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 10px">Vergütung: {{ $mission->preisFahrer }} €</td>
+                <td style="padding: 10px">Gutschrift-Nr.:  </td>
+                <td style="padding: 10px">Bezahlt am: </td>
+            </tr>
+        </table><hr>
+    <h3>Auftraggeber</h3>
 @endsection
