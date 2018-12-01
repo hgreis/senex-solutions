ddf2ff9ae24e0278f1940ca4b2e020eb56fe7bab
hgreis <heikogreis@justmail.de>
2018-12-01 15:30:28 +0100

    #05.11 update

--- resources/views/pages/menu_invoice.blade.php
+++ resources/views/pages/menu_invoice.blade.php
@@ -9,8 +9,8 @@
                 <button class="blackButton" onclick="window.location.href='/bill'" >Rechnung generieren</button><br>
                 <button class="blackButton" onclick="window.location.href='/bill'" >Auflistung-Gutschrift</button>
             <p>
-                <button class="brownButton" onclick="window.location.href='/credits/1'" >Gutschrift erstellen</button>
-                <button class="pinkButton" onclick="window.location.href='/credits/2'" >Gutschrift erstellen</button><br>
+                <button class="brownButton" onclick="window.location.href='/invoices/1'" >Rechnung - Übersicht</button>
+                <button class="pinkButton" onclick="window.location.href='/invoices/2'" >Rechnung - Übersicht</button><br>
                 <button class="brownButton" onclick="window.location.href='/invoicesPaid/1'" >Zahlungseingang</button>
                 <button class="pinkButton" onclick="window.location.href='/invoicesPaid/2'" >Zahlungseingang</button>
             </p>
@@ -18,6 +18,8 @@
         <div style="float: left; text-align: center; padding: 10px">
             <h2>Unternehmer-Gutschriften</h2>
             <button class="brownButton" onclick="window.location.href='/credits/1'" >Gutschrift erstellen</button>
-            <button class="pinkButton" onclick="window.location.href='/credits/2'" >Gutschrift erstellen</button>
+            <button class="pinkButton" onclick="window.location.href='/credits/2'" >Gutschrift erstellen</button><br>
+            <button class="brownButton" onclick="window.location.href=''" >Überweisung bestätigen</button>
+            <button class="pinkButton" onclick="window.location.href=''" >Überweisung bestätigen</button>
         </div>
 @endsection
