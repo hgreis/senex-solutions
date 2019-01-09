4d19aae1a403216dac7fd9fc67bd745c5df86049
hgreis <heikogreis@justmail.de>
2019-01-09 16:06:43 +0100

    #13.03' sudo git commit -m #13.03 ;

--- resources/views/pages/payCredits.blade.php
+++ resources/views/pages/payCredits.blade.php
@@ -7,10 +7,9 @@
 @endif
 
 		<h1 style="text-align: center">Gutschriften - Überweisung bestätigen</h1>
-		<table style="width: 70%">
-			<tr>
-				<th></th>
-				<th>#</th>
+		<table class="table" align="center" style="width: 80%">
+			<tr class="my1000">
+				<th style="text-align: center">#</th>
 				<th style="width: 90px">Datum</th>
 				<th>Unternehmer</th>
 				<th style="text-align: center">Netto</th>
@@ -18,15 +17,14 @@
 				<th></th>
 			</tr>
 			@foreach( $credits as $credit)
-				<tr>
+				<tr class="my1001">
 					<td style="text-align: center; width: 80px">
 						@if($credits->company == 2)
-							<a class="button" target="_blank" href="/Gutschriften/Sabine Heinrichs Transporte GS-{{$credit->number}}.pdf">Details</a>
+							<a class="button" target="_blank" href="/Gutschriften/Sabine Heinrichs Transporte GS-{{$credit->number}}.pdf">GS-{{ $credit->number }}</a>
 						@else
-							<a class="button" target="_blank" href="/Gutschriften/Strerath Transporte GS-{{$credit->number}}.pdf">Details</a>
+							<a class="button" target="_blank" href="/Gutschriften/Strerath Transporte GS-{{$credit->number}}.pdf">GS-{{ $credit->number }}</a>
 						@endif
 					</td>
-					<td>{{ $credit->number }} </td>
 					<td>{{ date_format(date_create($credit->date), 'd.m.Y') }}</td>
 					<td>{{ $credit->driver->name }}</td>
 					<td style="text-align: right; width: 80px">{{ number_format($credit->priceNet, 2, ',', ' ') }} €</td>
